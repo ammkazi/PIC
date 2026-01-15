aa4abfc7d26076f664d8ced8448bf92467f812da
ammkazi <amm.kazi@gmail.com>
2026-01-15 11:31:35 +0530

    Simple structure of C program

---
+++ p1.c
@@ -0,0 +1,28 @@
+/*
+ * Filename: p1.c
+ * Created Date: Thursday, January 15th 2026, 11:25:05 am
+ * Author: Aiman Kazi
+ *
+ * Copyright (c) 2026 Visual Labs
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
+
+/*
+1. #include - preprocessor directive
+2. <stdio.h> - standard i/o header file
+3. main - function - it is also the entry point of the program
+4. int - Written before main - I will return an integer value
+5. {} - Braces - we write block of code inside the braces
+6. printf() - called function - helps to print the output
+7. \n = escape sequence - takes the cursor to the new line
+8. We write statements inside the function
+9. ; (semi colon) is called is a statement terminator
+10. we compile the code using gcc compiler - (gcc p1.c)
+*/
